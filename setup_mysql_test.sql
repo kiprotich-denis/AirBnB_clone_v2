7593b8f606ffa990f50cda881cfa047a4b72f783
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-03-07 03:31:41 -0800

    MySQL setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- creates database hbnb_dev_db and a new user hbnb_dev in localhost
+--  password of hbnb_dev is set to be hbnb_dev_pwd
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
