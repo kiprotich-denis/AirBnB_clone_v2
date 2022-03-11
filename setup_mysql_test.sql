5d4f3e78503a6ca865485506c87be3425e0e590f
MahiET <meazalemma2020@gmail.com>
2022-03-11 06:26:42 -0800

    Mysql Setup Test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,7 @@
--- creates database hbnb_dev_db and a new user hbnb_dev in localhost
---  password of hbnb_dev is set to be hbnb_dev_pwd
+-- Script to create a MySQL server with the database hbnb_test_db.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
