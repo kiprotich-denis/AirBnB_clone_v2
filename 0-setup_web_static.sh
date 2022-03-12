297468d95f885fde89e96fde50a1bcba924c7b9a
MahiET <meazalemma2020@gmail.com>
2022-03-12 01:23:18 -0800

    setup Web page

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,35 @@
 #!/usr/bin/env bash
-# Script that sets up your web servers for the deployment of web_static.
-
-sudo apt-get update
+# web server for deployment
+sudo apt-get -y update
 sudo apt-get -y install nginx
+
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" >> /data/web_static/releases/test/index.html
+
+echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tBest School\n\t</body>\n</html>" >/data/web_static/releases/test/index.html
+
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "26i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+
+sudo chown -R ubuntu:ubuntu /data
+
+new_str="location /hbnb_static {\nalias /data/web_static/current;\n}"
+sudo sed -i "/# Only/ i $new_str" /etc/nginx/sites-enabled/default
+
+sudo service nginx restart#!/usr/bin/env bash
+# web server for deployment
+sudo apt-get -y update
+sudo apt-get -y install nginx
+
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+
+echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tBest School\n\t</body>\n</html>" >/data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data
+
+new_str="location /hbnb_static {\nalias /data/web_static/current;\n}"
+sudo sed -i "/# Only/ i $new_str" /etc/nginx/sites-enabled/default
+
 sudo service nginx restart
