e3c394918c3c08028a09a1c0ee80bbb46cf86d5d
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-03-24 16:07:46 -0700

    setup Web page

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,7 +9,7 @@ fi
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 
-echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tBest School\n\t</body>\n</html>" >/data/web_static/releases/test/index.html
+echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tBest School\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
