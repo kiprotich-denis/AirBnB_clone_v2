66f662ebbd786d690078141b396098fc1e21aff4
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-03-24 15:52:01 -0700

    setup Web page

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 # web server for deployment
-sudo apt-get -y update
-sudo apt-get -y install nginx
+if [ ! -x /usr/sbin/nginx ]
+then
+	sudo apt-get -y update
+	sudo apt-get -y install nginx
+fi
 
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
@@ -10,26 +13,9 @@ echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tBest School\n\t</body>\n</ht
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-sudo chown -R ubuntu:ubuntu /data
+sudo chown -R ubuntu:ubuntu "/data/"
 
-new_str="location /hbnb_static {\nalias /data/web_static/current;\n}"
-sudo sed -i "/# Only/ i $new_str" /etc/nginx/sites-enabled/default
-
-sudo service nginx restart#!/usr/bin/env bash
-# web server for deployment
-sudo apt-get -y update
-sudo apt-get -y install nginx
-
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-
-echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tBest School\n\t</body>\n</html>" >/data/web_static/releases/test/index.html
-
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-sudo chown -R ubuntu:ubuntu /data
-
-new_str="location /hbnb_static {\nalias /data/web_static/current;\n}"
+new_str="location /hbnb_static/ {\nalias /data/web_static/current;\n}"
 sudo sed -i "/# Only/ i $new_str" /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
