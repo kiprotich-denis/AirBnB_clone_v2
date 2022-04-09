d264bd962f67708e8381fe5854692ae622807214
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-04-09 04:40:45 -0700

    Router C

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -16,7 +16,7 @@ def hbnb():
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def text(text=None):
+def text(text):
     """Dynamic inputed text: replace _ for space and show text"""
     return "C {}".format(text.replace('_', ' '))
 
