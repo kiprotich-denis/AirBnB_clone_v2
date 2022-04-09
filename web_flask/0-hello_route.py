0a536c93ee56849edd4c3df0cc3cbb4824c20e25
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-04-09 04:48:54 -0700

    starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
-
+"""starts a Flask web application"""
 from flask import Flask
 app = Flask(__name__)
 
@@ -11,6 +8,7 @@ app = Flask(__name__)
 def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
