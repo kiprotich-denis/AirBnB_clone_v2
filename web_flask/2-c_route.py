5071d42a79fcf343ad7840789ffefdf9532e14c5
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-04-09 04:32:13 -0700

    Router C

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello():
     """Start a basic Flask web application"""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Adding a specific route /hbnb"""
     return 'HBNB'
 
 
-@app.route('/c/<string:text>')
+@app.route('/c/<text>', strict_slashes=False)
 def text(text=None):
     """Dynamic inputed text: replace _ for space and show text"""
     return "C {}".format(text.replace('_', ' '))
