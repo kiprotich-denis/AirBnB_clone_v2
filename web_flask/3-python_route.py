82031719a5d1ed6d0b71d239fb2291011ac38d84
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-04-09 04:45:51 -0700

    Python Router

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""starts a Flask web application"""
 from flask import Flask
 app = Flask(__name__)
 
@@ -16,7 +17,7 @@ def hbnb():
 
 
 @app.route('/c/<string:text>', strict_slashes=False)
-def c_text(text=None):
+def c_text(text):
     """Dynamic inputed text: replace _ for space and show text"""
     return "C {}".format(text.replace('_', ' '))
 
